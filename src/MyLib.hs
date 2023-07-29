883597fc1a82506610a60aaefe3eaec9180c6281
Matt Hunzinger <matthunz2@gmail.com>
2023-07-29 07:44:44 -0400

    Create Pitch and Octave types

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -7,7 +7,23 @@ import Data.Word (Word8)
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
+data Pitch = C | CSharp | D | DSharp | E | F | FSharp | G | GSharp | A | ASharp | B
+  deriving (Show, Enum, Bounded)
+
+data Octave = OctaveNeg1 | Octave0 | Octave1 | Octave2 | Octave3 | Octave4 | Octave5 | Octave6 | Octave7
+  deriving (Enum, Show)
+
 newtype MidiNote = MidiNote Word8
+
+midiNote :: Pitch -> Octave -> MidiNote
+midiNote pitch octave =
+  MidiNote $
+    (fromIntegral (fromEnum octave) + 1)
+      * (fromIntegral (fromEnum (maxBound :: Pitch)) + 1)
+      + fromIntegral (fromEnum pitch)
+
+pitch :: MidiNote -> Pitch
+pitch (MidiNote note) = toEnum . fromIntegral $ fromIntegral note `mod` (fromEnum (maxBound :: Pitch) + 1)
 
 newtype MidiSet = MidiSet Word128
 
